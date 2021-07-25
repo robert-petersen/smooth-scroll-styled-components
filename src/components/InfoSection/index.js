87689df04dcd74de6db8167dfe8d3dd7831fd484
Robert Petersen <robert.petersen808@gmail.com>
2021-07-25 16:17:50 -0500

    completed info section

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,21 +1,51 @@
 import React from 'react';
+import {Button} from "../ButtonElement";
+import { 
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  Column2,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  ImgWrap,
+  Img
+} from "./InfoElements";
 
-const InfoSection = () => {
+const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headLine, darkText, description, buttonLabel, img, alt, primary, dark, dark2}) => {
   return (
     <>
-      <InfoContainer id={id}>
+      <InfoContainer lightBg={lightBg} id={id}>
         <InfoWrapper>
-          <InfoRow>
+          <InfoRow imgStart={imgStart}>
             <Column1>
               <TextWrapper>
-                <TopLine>Topline</TopLine>
-                <Heading>Heading</Heading>
-                <Subtitle>Subitle</Subtitle>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headLine}</Heading>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
                 <BtnWrap>
-                  <Button to="home">Button</Button>
+                  <Button 
+                    to="home"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact={true}
+                    offset={-80}
+                    primary={primary ? 1 : 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+                    >{buttonLabel}</Button>
                 </BtnWrap>
               </TextWrapper>
             </Column1>
+            <Column2>
+              <ImgWrap>
+               <Img src={img} alt={alt}/>
+              </ImgWrap>
+            </Column2>
           </InfoRow>
         </InfoWrapper>
       </InfoContainer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
+import { homeObjOne, homeObjTwo, homeObjFour } from '../components/InfoSection/Data';
+import Services from '../components/Services';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,7 +18,10 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
-      <InfoSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <Services />
+      <InfoSection {...homeObjFour} />
     </>
   )
 }
